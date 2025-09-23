16074bbba6ab53b35d52120258ed15dd7f0976d9
Ary de Souza Oliveira Neto <arineto10@hotmail.com>
2025-09-23 10:59:34 -0300

    navbar terminada

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,24 +7,26 @@ import { logo, menu, close } from '../assets';
 
 
 const Navbar = () => {
-  useStateSnippe 
+   const [active, setActive] = useState('´')
   return (
     <nav
     className={`
-      ${styles.paddinX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}
+      ${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}
     >
-      <div className="w-full flex justify-between items-center max-w-7x1 mx-auto">
+      <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
         <Link
         to="/"
         className="flex items-center gap-2"
         onClick={() => {
           setActive("");
+          window.scrollTo(0, 0);
         }}
         >
+          <img src={logo} alt="logo" className="w-9 h-9 object-contain"/>
+          <p className="text-white text-[18px] font-bold cursor-pointer">Ary <span className="sm:block hidden">| de Souza</span></p>
         </Link>
-
+        <p className="text-pink-500">qweqwe</p>
       </div>
-
     </nav>
   )
 }
