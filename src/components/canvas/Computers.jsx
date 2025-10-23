c3709473e5113527f2796123f17601880fc7828f
Ary de Souza Oliveira Neto <arineto10@hotmail.com>
2025-10-23 16:39:37 -0300

    configurações pc

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -8,10 +8,22 @@ const Computers = () => {
 
   return (
     <mesh>
-      <hemisphereLight intensity={0.15} groundColor="black" />
-      <pointLight intensity={1}/>
+      <hemisphereLight intensity={5} 
+      groundColor="black" />
+      <pointLight intensity={2} />
+      <spotLight
+      position={[-20, 50, 10]}
+      angle={0.12}
+      penumbra={1}
+      intensity={1}
+      castShadow
+      shadow-mapSize={1024}
+      />
       <primitive
       object={computer.scene}
+      scale={0.75}
+      position={[0, -3.25, -1.5]}
+      rotation={[-0.01, -0.2, -0.1]}
       />
     </mesh>
   )
@@ -21,7 +33,7 @@ const ComputersCanvas = () => {
     <Canvas
       frameLoop="demand"
       shadows
-      camera={{ position: [20, 3, 5], fov: 25}}
+      camera={{ position: [20, 3, 5], fov: 25 }}
       gl={{ preserveDrawingBuffer: true }}
     >
       <Suspense fallback={<CanvasLoader />}>
